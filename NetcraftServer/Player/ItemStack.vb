94ed4bdf881ff1d17a657fda1fc0ad1659e76b61
GladCypress3030 <g.kikikam@gmail.com>
2020-12-20 18:42:04 +0300

    12345

--- NetcraftServer/Player/ItemStack.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class ItemStack
-    Property Type As Material
-    Property Count As Integer
-    Public Sub New(arg0 As Material, arg1 As Integer)
-        Type = arg0
-        Count = arg1
-    End Sub
-End Class
